e63b1f922af8f395a2d9a0e4515d3dcbc6906226
Wayne Rosing <wrosing@lco.global>
2020-08-29 17:01:12 +0000

    improved autofucus and skyflat

--- devices/sequencer.py
+++ devices/sequencer.py
@@ -562,7 +562,7 @@ class Sequencer:
         print('Autofocus Starting at:  ', foc_pos0, '\n\n')
         throw = 200  # NB again, from config.  Units are microns
         if not sim:
-            result = g_dev['cam'].expose_command(req, opt, no_AWS=True), ## , script = 'focus_auto_script_0')  #  This is where we start.
+            result = g_dev['cam'].expose_command(req, opt, no_AWS=True) ## , script = 'focus_auto_script_0')  #  This is where we start.
         else:
             result['FWHM'] = 3
             result['mean_focus'] = foc_pos0
@@ -572,10 +572,11 @@ class Sequencer:
         g_dev['foc'].focuser.Move((foc_pos0 - throw)*g_dev['foc'].micron_to_steps)
         #opt['fwhm_sim'] = 4.
         if not sim:
-            result = g_dev['cam'].expose_command(req, opt, no_AWS=True), ## , script = 'focus_auto_script_1')  #  This is moving in one throw.
+            result = g_dev['cam'].expose_command(req, opt, no_AWS=True) ## , script = 'focus_auto_script_1')  #  This is moving in one throw.
         else:
             result['FWHM'] = 4
             result['mean_focus'] = foc_pos0 - throw
+        breakpoint()
         spot2 = result['FWHM']
         foc_pos2 = result['mean_focus']
         print('Autofocus Overtaveling Out.\n\n')
@@ -584,7 +585,7 @@ class Sequencer:
         g_dev['foc'].focuser.Move((foc_pos0 + throw)*g_dev['foc'].micron_to_steps)
         #opt['fwhm_sim'] = 5
         if not sim:
-            result = g_dev['cam'].expose_command(req, opt, no_AWS=True), ## , script = 'focus_auto_script_2')  #  This is moving out one throw.
+            result = g_dev['cam'].expose_command(req, opt, no_AWS=True) ## , script = 'focus_auto_script_2')  #  This is moving out one throw.
         else:
             result['FWHM'] = 4.5
             result['mean_focus'] = foc_pos0 + throw
@@ -607,7 +608,7 @@ class Sequencer:
             print ('Moving to Solved focus:  ', round(d1, 2), ' calculated:  ',  new_spot)
             g_dev['foc'].focuser.Move(int(d1*g_dev['foc'].micron_to_steps))
             if not sim:
-                result = g_dev['cam'].expose_command(req, opt, no_AWS=True),  #   script = 'focus_auto_script_3')  #  This is verifying the new focus.
+                result = g_dev['cam'].expose_command(req, opt, no_AWS=True)  #   script = 'focus_auto_script_3')  #  This is verifying the new focus.
             else:
                 result['FWHM'] = new_spot
                 result['mean_focus'] = d1
