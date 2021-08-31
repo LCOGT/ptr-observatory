93d2ed9327861c1f2f3bf112b5623a97083dd955
Wayne Rosing <wrosing@lco.global>
2021-08-31 19:16:40 +0000

    Added bug list

---
+++ bug_feature_list.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 31 19:01:12 2021
+
+@author: obs
+"""
+
+#0831   Delete incoming commands if they are older than 1 hour.
+#         Implement Cancel or stop for exposures and running projects.
+#         Improve Camera status reporting
+#         Implement a very basic log
+#         Start recording the Weather
+        
