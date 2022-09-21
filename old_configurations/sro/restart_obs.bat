7c4225cbd1fb807b2542449b911faf0ac9889d82
Wayne Rosing <wrosing@lco.global>
2022-09-21 22:21:35 +0000

    Clean out a lot of cruft

--- old_configurations/sro/restart_obs.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd anaconda3
-start /B C:\Users\obs\anaconda3\python C:\Users\obs\Documents\GitHub\ptr-observatory\wema.py %*
-start /B C:\Users\obs\anaconda3\python C:\Users\obs\Documents\GitHub\ptr-observatory\obs.py %*
-pause
