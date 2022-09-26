d17b3f5fd73de24f9ed0fcd8d4d2cae03c4d1476
Wayne Rosing <wrosing@lco.global>
2022-09-26 20:15:49 +0000

    misc

--- devices/sequencer.py
+++ devices/sequencer.py
@@ -596,6 +596,9 @@ class Sequencer:
     def execute_block(self, block_specification):
         #ocn_status = eval(self.redis_server.get('ocn_status'))
         #enc_status = eval(self.redis_server.get('enc_status'))
+        print('|n|n Staring a new project!  \n')
+        print(block_specification, ' \n\n\n')
+        
         self.block_guard = True
         # NB we assume the dome is open and already slaving.
         block = copy.deepcopy(block_specification)
@@ -1677,6 +1680,7 @@ class Sequencer:
         x = [foc_pos2, foc_pos1, foc_pos3]
         y = [spot2, spot1, spot3]
         print('X, Y:  ', x, y, 'Desire center to be smallest.')
+  
         if spot1 is None or spot2 is None or spot3 is None or spot1 == False or spot2 == False or spot3 == False:  #New additon to stop crash when no spots
             print("No stars detected. Returning to original focus setting and pointing.")
 
