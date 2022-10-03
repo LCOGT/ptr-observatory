fc9661bea7606238d37c47f23d858348dd3be3c6
Wayne Rosing <wrosing@lco.global>
2022-10-03 17:49:45 +0000

    Update camera.py
    Co-Authored-By: mfitzasp <16806710+mfitzasp@users.noreply.github.com>

--- devices/camera.py
+++ devices/camera.py
@@ -2011,7 +2011,7 @@ class Camera:
                             err_dec = round((TARGDEC - DECJ2000)*3600, 1)
                             print("Focus images error in ra, dec, asec:  ", err_ha, err_dec)
                             #g_dev['mnt'].set_last_reference(err_ha, err_dec, time_now)
-                            if (err_ha > 100 or err_dec > 100 or err_ha < -100 or err_dec < -100) and self.config['mount']['mount1']['permissive_mount_reset'] == 'yes':
+                            if (err_ha > 1200 or err_dec > 1200 or err_ha < -1200 or err_dec < -1200) and self.config['mount']['mount1']['permissive_mount_reset'] == 'yes':
                                 g_dev['mnt'].reset_mount_reference()
                                 print ("I've reset the mount_reference")
                                 g_dev['mnt'].current_icrs_ra = solve['ra_j2000_hours']
@@ -2023,6 +2023,7 @@ class Camera:
                             #return result
                         except:
                             print(cal_path + cal_name, "  was not solved, sorry!")
+                            print(traceback.format_exc())
                         ##    #g_dev['mnt'].reset_last_reference()
                             #return result
                            #Return to classic processing
