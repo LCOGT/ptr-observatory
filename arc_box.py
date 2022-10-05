4eb5e90bef1f1fc06aeda2c043bdfef0fc0e807b
darrenhunt2 <darren.e.hunt@gmail.com>
2022-10-05 13:25:24 -0400

    Some fixes to incomplete code

--- arc_box.py
+++ arc_box.py
@@ -1,55 +1,58 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Aug  5 21:19:46 2021
+Created on Thu Aug 5 21:19:46 2021
 
 @author: obs
 """
-
-    def __init__(self, com_port):
-        self.thorium_mirror = bytes([0x0d, 0x01, 0x42, 0xa0, 0x10])
-        self.tungsten_mirror = bytes([0x0d, 0x01, 0x42, 0x90, 0x20])
-        self.blue_mirror = bytes([0x0d, 0x01, 0x42, 0xc0, 0xf0])
-        self.white_mirror = bytes([0x0d, 0x01, 0x42, 0xd0, 0xe0]) #Combining Blue and Tungsten_halogen
-        self.solar_mirror = bytes([0x0d, 0x01, 0x42, 0x90, 0x20])  #Wrong spec, just a placeholder.
-        self.dark_mirror = bytes([0x0d, 0x01, 0x42, 0x80, 0x30])   #Mirror in AGU is deployed, No Light
-        self.no_light_no_mirror = bytes([0x0d, 0x01, 0x42, 0x00, 0xb0])    #Mirror in AGU out of the way
-        self.arc_status = 'unknown'
-        self.com_port = com_port
-        if com_port is not None:
-            #self.set_arc_box('Reset')
-            pass
+import serial
 
 
-    def set_arc_box(self, cmd):
-        breakpoint()
-        self._com = serial.Serial(self.com_port, baudrate=2400, bytesize=8, \
-                                  parity='N', stopbits=1, timeout=0.1)
-        if cmd == "Thorium":
-            cmd_arc = self.thorium_mirror
-        elif cmd == "Tungsten":
-            cmd_arc = self.tungsten_mirror
-        elif cmd == "Blue":
-            cmd_arc = self.blue_mirror
-        elif cmd == "White":
-            cmd_arc = self.white_mirror
-        elif cmd == "Solar":
-            cmd_arc = self.solar_mirror
-        elif cmd == 'Dark_mirror':
-            cmd_arc = self.dark_mirror
-        elif cmd == 'Object' or cmd == 'Reset':
-            cmd_arc = self.no_light_no_mirror
-        self._com.write(cmd_arc*2)
-        self.arc_status = cmd
-        self._com.close()
-        print('Arc_box:  ', cmd)
+thorium_mirror = bytes([0x0D, 0x01, 0x42, 0xA0, 0x10])
+tungsten_mirror = bytes([0x0D, 0x01, 0x42, 0x90, 0x20])
+blue_mirror = bytes([0x0D, 0x01, 0x42, 0xC0, 0xF0])
+white_mirror = bytes(
+    [0x0D, 0x01, 0x42, 0xD0, 0xE0]
+)  # Combining Blue and Tungsten_halogen
+solar_mirror = bytes([0x0D, 0x01, 0x42, 0x90, 0x20])  # Wrong spec, just a placeholder.
+dark_mirror = bytes(
+    [0x0D, 0x01, 0x42, 0x80, 0x30]
+)  # Mirror in AGU is deployed, No Light
+no_light_no_mirror = bytes(
+    [0x0D, 0x01, 0x42, 0x00, 0xB0]
+)  # Mirror in AGU out of the way
+arc_status = "unknown"
 
 
-    def arc_lamp_status(self):
-        return self.arc_status
+def set_arc_box(com_port, cmd):
+    """Sets calibration lamps to a specified configuration."""
+
+    _com = serial.Serial(
+        com_port, baudrate=2400, bytesize=8, parity="N", stopbits=1, timeout=0.1
+    )
+    if cmd == "Thorium":
+        cmd_arc = thorium_mirror
+    elif cmd == "Tungsten":
+        cmd_arc = tungsten_mirror
+    elif cmd == "Blue":
+        cmd_arc = blue_mirror
+    elif cmd == "White":
+        cmd_arc = white_mirror
+    elif cmd == "Solar":
+        cmd_arc = solar_mirror
+    elif cmd == "Dark_mirror":
+        cmd_arc = dark_mirror
+    elif cmd == "Object" or cmd == "Reset":
+        cmd_arc = no_light_no_mirror
+    else:
+        print("Arc box command not recognized.")
+
+    _com.write(cmd_arc * 2)
+    arc_status = cmd
+    _com.close()
+    print("Arc_box:  ", cmd)
 
 
-
-if __name__ == '__main__':
-
-    arc_lamp = ArcLampBox('COM11')
-    arc_lamp.set_arc_box("White")   #This shoulld turn on White light and Blue LED
+def arc_lamp_status():
+    # This part needs a redo. Placeholder
+    if arc_status is not None:
+        return arc_status
