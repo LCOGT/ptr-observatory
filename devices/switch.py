514f6f034be3c0b3648284162c6bcff639ed06e3
darrenhunt2 <darren.e.hunt@gmail.com>
2022-10-05 17:50:32 -0400

    Fix typo

--- devices/switch.py
+++ devices/switch.py
@@ -1,8 +1,7 @@
-
 import win32com.client
 
-class Switch:
 
+class Switch:
     def __init__(self, driver: str, name: str):
         self.name = name
         self.switch = win32com.client.Dispatch(driver)
@@ -12,14 +11,14 @@ class Switch:
         print(self.switch.Description)
 
     def get_status(self):
-        status = {"device_type":"switch"}
+        status = {"device_type": "switch"}
         return status
 
     def parse_command(self, command):
-        req = command['required_params']
-        opt = command['optional_params']
-        action = command['action']
-        
+        req = command["required_params"]
+        opt = command["optional_params"]
+        action = command["action"]
+
         if action == "on":
             self.on_command(req, opt)
         elif action == "off":
@@ -29,20 +28,21 @@ class Switch:
         else:
             print(f"Command <{action}> not recognized.")
 
-
     ##############################
     #       Switch Commands      #
     ##############################
 
     def on_command(self, req: dict, opt: dict):
-        ''' set the switch to `on` '''
+        """set the switch to `on`"""
         print(f"switch cmd: on")
         pass
+
     def off_command(self, req: dict, opt: dict):
-        ''' set the switch to `off` '''
+        """set the switch to `off`"""
         print(f"switch cmd: off")
         pass
-    def pulse_command(self, req: dict, opt:dict):
-        ''' set the switch to `pulse` '''
-        pritn(f"switch cmd: pulse")
+
+    def pulse_command(self, req: dict, opt: dict):
+        """set the switch to `pulse`"""
+        print(f"switch cmd: pulse")
         pass
