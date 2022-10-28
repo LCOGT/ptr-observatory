5e358fc864b1edd4032c9ae17e4c733ea3b65b3d
Wayne Rosing <wrosing@lco.global>
2022-10-28 14:40:23 +0000

    Update site_config.py

--- configs/mrc/site_config.py
+++ configs/mrc/site_config.py
@@ -83,7 +83,7 @@ site_config = {
     'site_is_specific':  False,  # Indicates some special code for this site, found at end of config.
 
 
-    'host_wema_site_name':  'SRO',  #  The umbrella header for obsys in close geographic proximity.
+    'host_wema_site_name':  'MRC',  #  The umbrella header for obsys in close geographic proximity.
     'name': 'Mountain Ranch Camp Observatory 0m35f7.2',
     'airport_code': 'SBA',
     'location': 'Near Santa Barbara CA,  USA',
