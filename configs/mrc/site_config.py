d041ce95ed42f93e318d65ddc8d505e00d1ba457
Wayne Rosing <wrosing@lco.global>
2022-12-05 23:23:07 +0000

    Setup time offsets chg in site_config

--- configs/mrc/site_config.py
+++ configs/mrc/site_config.py
@@ -121,7 +121,7 @@ site_config = {
     
     'auto_midnight_moonless_bias_dark': False,
     'auto_eve_sky_flat': False,
-    'eve_sky_flat_sunset_offset': -60.,  #  Minutes  neg means before, + after.
+    'eve_sky_flat_sunset_offset': -180.,  #  Minutes  neg means before, + after.
     'eve_cool_down_open' : -60.0,
     'auto_morn_sky_flat': False,
     'auto_morn_bias_dark': True,
