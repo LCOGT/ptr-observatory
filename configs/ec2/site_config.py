ee3e09dfbd828243945d3e81e61c63fd80438687
mfitzasp <mfitzasp@gmail.com>
2023-03-05 23:01:55 +0000

    ec2temp

--- configs/ec2/site_config.py
+++ configs/ec2/site_config.py
@@ -706,7 +706,7 @@ site_config = {
                 'crop_preview_ytop': 2,
                 'crop_preview_xleft': 2,
                 'crop_preview_xright': 2,
-                'temp_setpoint': -20,    #Verify we can go colder, this system has a chiller
+                'temp_setpoint': -4,    #Verify we can go colder, this system has a chiller
                 'has_chiller': True,
                 'calib_setpoints': [-20, -20, -20, -20, -20, -20, \
                                     -20, -20, -20, -20, -20, -20],  #  Picked by month-of-year 
