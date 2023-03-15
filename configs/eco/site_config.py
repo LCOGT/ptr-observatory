67aea357ea57b6bc58126f5efd497b4ee26d6d2f
Michael Fitzgerald <mfitzasp@gmail.com>
2023-03-15 01:43:45 +0000

    eco day_warm settings

--- configs/eco/site_config.py
+++ configs/eco/site_config.py
@@ -546,7 +546,8 @@ site_config = {
                 'crop_preview_xright': 1,
                 'temp_setpoint': -20,   
                 #'calib_setpoints': [-35,-30, -25, -20, -15, -10 ],  #  Should vary with season?
-                'day_warm': False,
+                'day_warm': True,
+                'day_warm_degrees' : 8, # Number of degrees to warm during the daytime.
                 'cooler_on': True,
                 
                 "cam_needs_NumXY_init": False,
