0cc538090e1a15255bbbdbe826b96d0fac424c56
Michael Fitzgerald <mfitzasp@gmail.com>
2023-06-14 23:40:43 +0000

    Update GetIndexFiles.bat

--- support_info/Installers/WCSIndexFiles/GetIndexFiles.bat
+++ support_info/Installers/WCSIndexFiles/GetIndexFiles.bat
@@ -1,1 +1,5 @@
 wget -c --tries=inf https://s3.amazonaws.com/www.photonranch.org/Kepler.zip
+tar -xf .\Kepler.zip
+rmdir /s /q "%HOMEPATH%/Documents/Kepler"
+move Kepler "%HOMEPATH%/Documents/"
+del Kepler.zip
