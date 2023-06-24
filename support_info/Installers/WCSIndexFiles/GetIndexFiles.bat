f9a0ac1f29c77b1d711529c9928c7482d9670630
mfitzasp <mfitzasp@gmail.com>
2023-06-23 21:24:01 -0700

    remove files

--- support_info/Installers/WCSIndexFiles/GetIndexFiles.bat
+++
@@ -1,5 +0,0 @@
-wget -c --tries=inf https://s3.amazonaws.com/www.photonranch.org/Kepler.zip --no-check-certificate
-tar -xf .\Kepler.zip
-rmdir /s /q "%HOMEPATH%/Documents/Kepler"
-move Kepler "%HOMEPATH%/Documents/"
-del Kepler.zip
