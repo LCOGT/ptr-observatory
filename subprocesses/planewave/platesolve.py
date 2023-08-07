74961ff1acbe83c658b63b96a14664bbb5682841
Wayne Rosing <wrosing@lco.global>
2023-08-07 08:41:21 +0000

    Update platesolve.py

--- subprocesses/planewave/platesolve.py
+++ subprocesses/planewave/platesolve.py
@@ -82,7 +82,7 @@ def platesolve(image_file, arcsec_per_pixel):
                 stdout=None,
                 stderr=PIPE
                 )
-        (stdout, stderr) = process.communicate(timeout=60)  # Obtain stdout and stderr output from the wcs tool
+        (stdout, stderr) = process.communicate(timeout=30)  # Obtain stdout and stderr output from the wcs tool
         exit_code = process.wait() # Wait for process to complete and obtain the exit code
         failed = False
         time.sleep(1)
