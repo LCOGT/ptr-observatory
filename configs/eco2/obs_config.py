6bd271914b87fbebd0d65ed1fd4c2f2c71052def
mfitzasp <mfitzasp@gmail.com>
2023-08-13 15:05:54 -0700

    Update obs_config.py

--- configs/eco2/obs_config.py
+++ configs/eco2/obs_config.py
@@ -754,7 +754,7 @@ site_config = {
                 'min_subframe': [128,128],
                 #'bin_modes':  [['Optimal', 0.91], ['Fine', 0.61], ['Coarse', 1.2], ['Eng', 0.30]],     #Meaning fixed binning if list has only one entry
                 'camera_gain':   2.417, #[10., 10., 10., 10.],     #  One val for each binning.
-                'camera_gain_stdev':   0.011, #[10., 10., 10., 10.],     #  One val for each binning.
+                'camera_gain_stdev':   0.11, #[10., 10., 10., 10.],     #  One val for each binning.
                 'read_noise':  10.224, #[9, 9, 9, 9],    #  All SWAGs right now
                 'read_noise_stdev':   0.003, #[10., 10., 10., 10.],     #  One val for each binning.
                 'reference_dark': 0.2,  #  NB  Guess
