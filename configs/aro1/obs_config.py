57d2b6584a74f93a4db2a3709caa51261329e081
Wayne Rosing <wrosing@lco.global>
2023-09-08 02:28:35 +0000

    Update obs_config.py

--- configs/aro1/obs_config.py
+++ configs/aro1/obs_config.py
@@ -58,7 +58,7 @@ site_config = {
     'lowest_requestable_altitude': -5,  # Degrees. For normal pointing requests don't allow requests to go this low.
     'degrees_to_avoid_zenith_area_for_calibrations': 0, 
     'temperature_at_which_obs_too_hot_for_camera_cooling' : 23, # NB NB WER ARO Obs has a chiller
-    
+
     # These are the default values that will be set for the obs
     # on a reboot of obs.py. They are safety checks that 
     # can be toggled by an admin in the Observe tab.
@@ -125,7 +125,7 @@ site_config = {
      'auto_morn_bias_dark': True,
      
      # FOCUS OPTIONS
-     'periodic_focus_time': 4.0, # This is a time, in hours, over which to bypass automated focussing (e.g. at the start of a project it will not refocus if a new project starts X hours after the last focus)
+     'periodic_focus_time': 12.0, # This is a time, in hours, over which to bypass automated focussing (e.g. at the start of a project it will not refocus if a new project starts X hours after the last focus)
      'stdev_fwhm': 0.5,  # This is the expected variation in FWHM at a given telescope/camera/site combination. This is used to check if a fwhm is within normal range or the focus has shifted
      'focus_trigger': 0.6,  # What FWHM increase is needed to trigger an autofocus
      
