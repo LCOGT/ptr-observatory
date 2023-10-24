01f7c021c09ec58d6cd27d11e938bf407d6e6ead
Matt Daily <mdaily@lco.global>
2023-10-24 14:36:24 -0700

    Add user ID to ingester payload, so we can include it in Frame record and filter on it
    This was done by overriding the base _create_header_data and subclassing the HeaderData class to extend the archive frame data to include the USERID. Following this change, changes will need to be made to the ptr science archive to accept this additional keyword, and a database migration will need to be made.

--- archive_file_input_classes.py
+++ archive_file_input_classes.py
@@ -1,5 +1,17 @@
-from ocs_archive.input.file import File, DataFile
+from ocs_archive.input.file import File, DataFile, FileSpecificationException
+from ocs_archive.input.headerdata import HeaderData
 from ocs_archive.settings import settings
+
+class PtrHeaderData(HeaderData):
+    def get_archive_frame_data(self):
+        archive_frame_data = super().get_archive_frame_data()
+        # add additional metadata as neeeded
+        archive_frame_data['user_id'] = self.get_user_id()
+        return archive_frame_data
+    
+    def get_user_id(self):
+        return self.get_headers().get('USERID', '')
+
 
 class PtrBaseFileClass(DataFile):
     def __init__(self, open_file: File, file_metadata: dict = None, blacklist_headers: tuple = settings.HEADER_BLACKLIST, required_headers: tuple = settings.REQUIRED_HEADERS):
@@ -25,3 +37,10 @@ class PtrBaseFileClass(DataFile):
                 if extension:
                     # Remove any extensions for the related frames, since the archive expects that
                     self.header_data.update_headers({key: basename})
+
+    def _create_header_data(self, file_metadata: dict):
+        if self._is_valid_file_metadata(file_metadata):
+            self.header_data = PtrHeaderData(file_metadata)
+            return
+        # Missing one or more required headers in the input file_metadata
+        raise FileSpecificationException('Could not find required keywords in headers!')
