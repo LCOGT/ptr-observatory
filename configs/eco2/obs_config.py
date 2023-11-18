91a8d022927255f15bf1bab2b6d10f16f3303bd0
mfitzasp <mfitzasp@gmail.com>
2023-11-18 11:17:04 +0000

    Update obs_config.py

--- configs/eco2/obs_config.py
+++ configs/eco2/obs_config.py
@@ -129,7 +129,7 @@ site_config = {
     
     # Exposure times for standard system exposures
     'focus_exposure_time': 15,  # Exposure time in seconds for exposure image
-    'pointing_exposure_time': 20,  # Exposure time in seconds for exposure image
+    'pointing_exposure_time': 10,  # Exposure time in seconds for exposure image
 
     # How often to do various checks and such
     'observing_check_period': 1,    # How many minutes between weather checks
