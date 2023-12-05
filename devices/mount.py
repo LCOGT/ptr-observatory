2b97b7b719cdd1f7beb7f02101487fb63fd49110
mfitzasp <mfitzasp@gmail.com>
2023-12-06 09:45:54 +1100

    Update mount.py

--- devices/mount.py
+++ devices/mount.py
@@ -2163,7 +2163,7 @@ class Mount:
             home_alt = self.settings["home_altitude"]
             home_az = self.settings["home_azimuth"]
             g_dev['obs'].time_of_last_slew=time.time()
-            g_dev['mnt'].go_command(alt=home_alt,az= home_az, skip_open_test=True)
+            g_dev['mnt'].go_command(alt=home_alt,az= home_az, skip_open_test=True, skyflatspot=True)
 
             self.wait_for_slew()
         self.wait_for_slew()
