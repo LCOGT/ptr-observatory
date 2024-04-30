69e955f6218848c932db2cfd3f0a7288009426eb
Wayne Rosing <wrosing@lco.global>
2024-04-30 20:46:20 +0000

    Update darkslide.py

--- devices/darkslide.py
+++ devices/darkslide.py
@@ -7,6 +7,7 @@ Created on Sat Aug 15 19:19:56 2020
 
 import serial
 from global_yard import g_dev
+import traceback
 
 
 class Darkslide(object):
@@ -17,19 +18,31 @@ class Darkslide(object):
         g_dev['drk'] = self
 
     def openDarkslide(self):
-        self._com = serial.Serial(self.com_port, timeout=0.3)
-        self._com.write(b'@')
-        self.slideStatus = 'Open'
-        self._com.close()
-        print("Darkslide Opened.")
+        try:
+            self._com = serial.Serial(self.com_port, timeout=0.3)
+            self._com.write(b'@')
+            self.slideStatus = 'Open'
+            self._com.close()
+            print("Darkslide Opened.")
+            return True
+        except:
+            print(traceback.format_exc())
+            print ("ALERT: DARKSLIDE EXCEPTION ON OPENING")
+            return False
     
     def closeDarkslide(self):
-        self._com = serial.Serial(self.com_port, timeout=0.3)   #Com 12 for saf, needs fixing.
-        self._com.write(b'A')
-        self.slideStatus = 'Closed'
-        self._com.close()
-        print("Darkslide Closed.")
-       
+        try:
+            self._com = serial.Serial(self.com_port, timeout=0.3)   #Com 12 for saf, needs fixing.
+            self._com.write(b'A')
+            self.slideStatus = 'Closed'
+            self._com.close()
+            print("Darkslide Closed.")
+            return True
+        except:
+            print(traceback.format_exc())
+            print ("ALERT: DARKSLIDE EXCEPTION ON CLOSING")
+            return False
+           
     def darkslideStatus(self):
         return self.slideStatus
    
