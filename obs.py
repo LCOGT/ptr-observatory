f54bc0984b8d265f89d0cdb60b4272a9016d9db6
mfitzasp <mfitzasp@gmail.com>
2024-05-24 12:54:00 +1000

    Update obs.py

--- obs.py
+++ obs.py
@@ -2582,7 +2582,7 @@ class Observatory:
                                                             try:
                                                                 print ("HA going in " + str(mount_deviation_ha))
                                                                 g_dev["mnt"].record_mount_reference(
-                                                                    mount_deviation_ha , mount_deviation_dec, pointing_ra, pointing_dec
+                                                                    -mount_deviation_ha , -mount_deviation_dec, pointing_ra, pointing_dec
                                                                 )
 
                                                             except Exception as e:
@@ -2591,7 +2591,7 @@ class Observatory:
                                                             try:
                                                                 print ("HA going in " + str(mount_deviation_ha))
                                                                 g_dev["mnt"].record_flip_reference(
-                                                                    mount_deviation_ha , mount_deviation_dec, pointing_ra, pointing_dec
+                                                                    -mount_deviation_ha , -mount_deviation_dec, pointing_ra, pointing_dec
                                                                 )
                                                             except Exception as e:
                                                                 plog("Something is up in the mount reference adjustment code ", e)
