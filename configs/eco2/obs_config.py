e494edf30c2ed6ad748de90251f4ddf3255994b2
mfitzasp <mfitzasp@gmail.com>
2024-09-10 10:24:48 +0000

    glitch fixes

--- configs/eco2/obs_config.py
+++ configs/eco2/obs_config.py
@@ -354,7 +354,7 @@ site_config = {
             'focuser_movement_settle_time': 10,
             'start_at_config_reference': False,
             'correct_focus_for_temperature' : True,
-            'maximum_good_focus_in_arcsecond': 3.5, # highest value to consider as being in "good focus". Used to select last good focus value
+            'maximum_good_focus_in_arcsecond': 4.0, # highest value to consider as being in "good focus". Used to select last good focus value
             'reference':24200,    #  20210313  Nominal at 10C Primary temperature
             'minimum': 0,     #  NB this area is confusing steps and microns, and need fixing.
             'maximum': 30000,   #12672 actually
